7c0c19cbae0da48e59650c86aaf28c5f8cd4cbc3
IMS <jose8055@gmail.com>
2019-02-18 16:22:06 -0500

    Initial Commit

---
+++ dist/auth.js
@@ -0,0 +1,35 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const OktaJwtVerifier = require('@okta/jwt-verifier');
+const oktaJwtVerifier = new OktaJwtVerifier({
+    clientId: '{YourClientId}',
+    issuer: 'https://dev-427522.oktapreview.com/oauth2/default'
+});
+function oktaAuth(req, res, next) {
+    return __awaiter(this, void 0, void 0, function* () {
+        try {
+            const token = req.token;
+            if (!token) {
+                return res.status(401).send('Not Authorised');
+            }
+            const jwt = yield oktaJwtVerifier.verifyAccessToken(token);
+            req.user = {
+                uid: jwt.claims.uid,
+                email: jwt.claims.sub
+            };
+            next();
+        }
+        catch (err) {
+            return res.status(401).send(err.message);
+        }
+    });
+}
+exports.oktaAuth = oktaAuth;
---
+++ src/auth.ts
@@ -0,0 +1,26 @@
+import { Request, Response, NextFunction} from 'express';
+
+const OktaJwtVerifier = require('@okta/jwt-verifier');
+
+const oktaJwtVerifier = new OktaJwtVerifier({
+  clientId: '0oajfcdjtzskVg5wp0h7',
+  issuer: 'https://dev-427522.oktapreview.com/oauth2/default'
+});
+
+export async function oktaAuth(req:Request, res:Response, next:NextFunction) {
+  try {
+    const token = (req as any).token;
+    if (!token) {
+      return res.status(401).send('Not Authorised');
+    }
+    const jwt = await oktaJwtVerifier.verifyAccessToken(token);
+    req.user = {
+      uid: jwt.claims.uid,
+      email: jwt.claims.sub
+    };
+    next();
+  }
+  catch (err) {
+    return res.status(401).send(err.message);
+  }
+}
